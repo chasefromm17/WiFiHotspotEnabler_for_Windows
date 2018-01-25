272beca382769846ee16911f57ba227ac4c0b567
Chase Fromm (crazy psycho!/flamboyant asshat!) <32696337+chasefromm17@users.noreply.github.com>
2018-01-24 19:22:21 -0600

    Add files via upload

---
+++ enable_chasefromm_wifi.bat
@@ -0,0 +1,3 @@
+netsh wlan set hostednetwork mode=allow ssid=chasefromm key=chasefromm
+netsh wlan start hostednetwork
+pause
---
+++ enable_wifi_hotspot.bat
@@ -0,0 +1,3 @@
+netsh wlan set hostednetwork mode=allow ssid=ChasesWiFi key=chasefromm
+netsh wlan start hostednetwork
+pause
