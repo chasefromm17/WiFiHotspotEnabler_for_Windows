c9ce98e6b034c4fdd9d578fa6b6b2dff4821ebad
Chase Fromm (crazy psycho!/flamboyant asshat!) <32696337+chasefromm17@users.noreply.github.com>
2018-01-24 19:22:53 -0600

    Update enable_chasefromm_wifi.bat

--- enable_chasefromm_wifi.bat
+++ enable_chasefromm_wifi.bat
@@ -1,3 +1,3 @@
-netsh wlan set hostednetwork mode=allow ssid=chasefromm key=chasefromm
+netsh wlan set hostednetwork mode=allow ssid=MY_HOME_NETWORK key=MY_HOME_NETWORK
 netsh wlan start hostednetwork
 pause
